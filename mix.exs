bb54d2d9239a8b26dc6e1ef85ff70add5ad39439
romulogarofalo <romulogarofalo@gmail.com>
2023-01-03 22:15:21 -0300

    general configs with docker

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Payfy.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:oban, "~> 2.12"}
     ]
   end
 
