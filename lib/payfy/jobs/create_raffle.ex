8edd6c444da74d4d9cafbdeb057956d5f60f409d
romulogarofalo <romulogarofalo@gmail.com>
2023-01-09 22:40:24 -0300

    add job of create raffle

--- lib/payfy/jobs/create_raffle.ex
+++ lib/payfy/jobs/create_raffle.ex
@@ -1,14 +1,16 @@
 defmodule Payfy.Jobs.CreateRaffle do
-
   use Oban.Worker, queue: "default", max_attempts: 3
 
   @impl Oban.Worker
   def perform(%Oban.Job{
-    args: %{
-      "name" => name,
-      "limit_date" => limit_date
-    }
-  }) do
-    Payfy.Raffle.Create.run(%{name: name, limit_date: limit_date})
+        args: %{
+          "name" => name,
+          "limit_date" => limit_date,
+          "id_join_raffle" => id_join_raffle
+        }
+      }) do
+    Payfy.Raffle.create_changeset(%{name: name, limit_date: limit_date})
+    |> Payfy.Raffle.add_id_join_raffle(id_join_raffle)
+    |> Payfy.Repo.insert() # passar esse insert para arquivo responsavel pela insercao?
   end
 end
