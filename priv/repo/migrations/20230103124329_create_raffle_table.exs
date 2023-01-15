c702c2c26c54cfae2ed47f4c9f68d69de3ed8d97
romulogarofalo <romulogarofalo@gmail.com>
2023-01-15 08:15:58 -0300

    update migrations

--- priv/repo/migrations/20230103124329_create_raffle_table.exs
+++ priv/repo/migrations/20230103124329_create_raffle_table.exs
@@ -2,10 +2,10 @@ defmodule Payfy.Repo.Migrations.CreateRaffleTable do
   use Ecto.Migration
 
   def change do
-    create table("raffles") do
+    create table("raffles", primary_key: false) do
+      add :id, :uuid, primary_key: true, null: false, default: fragment("gen_random_uuid()")
       add :name, :string
       add :limit_date, :date
-      add :id_join_raffle, :binary
       add :winner, :string
     end
   end
--- priv/repo/migrations/20230103144409_create_users_raffles_table.exs
+++ priv/repo/migrations/20230103144409_create_users_raffles_table.exs
@@ -4,7 +4,7 @@ defmodule Payfy.Repo.Migrations.CreateUsersRafflesTable do
   def change do
     create table("user_raffle", primary_key: false) do
       add(:user_id, references(:users, on_delete: :delete_all), primary_key: true)
-      add(:raffle_id, references(:raffles, on_delete: :delete_all), primary_key: true)
+      add(:raffle_id, references(:raffles, on_delete: :delete_all, type: :uuid), primary_key: true)
     end
 
     create index(:user_raffle, [:user_id])
